95a0f63cf2cc2a5c6b0b237dcdaa5e0b072f4746
Abuzeker <t.zikai@hotmail.com>
2023-11-06 17:11:21 +0800

    hmi1

---
+++ fgv-kop/.history/src/pages/KOPFRA4/Chilled_20231106170251.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChilledHMI from '../../HMI/KOPFRA4/ChilledHMI'
+
+const Chilled = () => {
+  return (
+    <div>
+        <ChilledHMI/>
+    </div>
+  )
+}
+
+export default Chilled
